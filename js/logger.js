455211569b9cfc23b4fcb01633f9c20e128c6f24
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2019-07-11 19:42:41 +0800

    Update logger.js

--- js/logger.js
+++ js/logger.js
@@ -1,1 +1,77 @@
-/*Dirk.L.Nicolaas*/function logHtml(t){var e=(t=t||!0)?"block":"none",o=document.createElement("span");o.id="sp",o.style="font-size:12px;z-index:1000";var n,i,l=document.createElement("div");l.id="log",l.style=`position:fixed;bottom:0;left:0;overflow: auto;height: 35px;display:${e};z-index:10;background-color:rgba(192, 192, 192, 0.8);  `,l.onclick=function(){"35px"===this.style.height?(this.style.height="15px",this.style.width="25px",this.style.backgroundColor="red",this.title="click to expand Logger",o.style.opacity="0"):(this.style.height="35px",this.style.width="auto",this.style.backgroundColor="rgba(192, 192, 192, 0.8)",o.style.opacity="1")},l.appendChild(o),document.body.appendChild(l),n="#log::-webkit-scrollbar {width: 5px;height: 0px;}#log::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);border-radius: 10px;}#log::-webkit-scrollbar-thumb {border-radius: 3px;-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5);}",(i=document.createElement("style")).textContent=n,document.head.appendChild(i),console||(console={});var r=console.log;console.log=function(t){l.style.display=e;var n=l.scrollHeight-l.clientHeight<l.scrollTop+1;o.innerHTML+="object"==typeof t?(JSON&&JSON.stringify?JSON.stringify(t):String(t))+"<br/>":t+"<br />",n&&(l.scrollTop=l.scrollHeight-l.clientHeight)},r()}//;window.onload=function(){return logHtml()};
+//window.onload = function(){return logHtml()};
+function logHtml(display) {
+  function insertStyle(sty){
+    var style = document.createElement("style");
+    style.textContent = sty;
+    document.head.appendChild(style);
+  };
+
+  var logstyle = `
+    #log::-webkit-scrollbar {width: 5px;height: 0px;}
+    #log::-webkit-scrollbar-track {-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);border-radius: 10px;}
+    #log::-webkit-scrollbar-thumb {border-radius: 3px;-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5);}
+  `;
+  display = display||true;
+  var disp = display?"block":"none";
+  
+  var span = document.createElement("span");
+  span.id = "sp";
+  span.style = "font-size:12px;z-index:1000";
+  
+  var logger = document.createElement("div");
+  logger.id = "log";
+  logger.style = `
+    position:fixed;
+    bottom:0;
+    left:0;
+    /*overflow: hidden;*/
+    overflow: auto;
+    height: 35px;
+    display:${disp};
+    z-index:10;
+    background-color:rgba(192, 192, 192, 0.8);
+  `;
+    logger.onclick=function(){
+      if(this.style.height === "35px"){
+        this.style.height = "15px";
+        this.style.width = "25px";
+        this.style.backgroundColor = "red";
+        this.title = "click to expand Logger";
+        span.style.opacity = "0";
+        
+        
+      }
+      else{
+        this.style.height = "35px";
+        this.style.width = "auto";
+        this.style.backgroundColor ="rgba(192, 192, 192, 0.8)";
+        span.style.opacity = "1";
+      }
+    }
+  logger.appendChild(span);
+  document.body.appendChild(logger);
+  insertStyle(logstyle);
+  
+  if (!console) {console = {};}
+  var consoleLogOld = console.log;
+  
+  
+  console.log = function (message) {
+      logger.style.display=disp;
+      var ToBottom = logger.scrollHeight - logger.clientHeight < logger.scrollTop + 1;
+      if (typeof message == 'object') {
+          
+          span.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br/>';
+          
+          //}            
+      } else {
+          span.innerHTML += message + '<br />';
+          
+      }
+      if (ToBottom) {
+        logger.scrollTop = logger.scrollHeight - logger.clientHeight;
+      }
+  
+  }
+  consoleLogOld();
+}
